146c6965e5e6bf0f47c631dc3e6fb2e9b376bd0b
sebaao <saburto11@gmail.com>
2024-03-01 17:59:57 -0300

    [ADD] - Estilos Navbar & Profile

--- src/components/MisClanesProfile.jsx
+++ src/components/MisClanesProfile.jsx
@@ -1,27 +1,33 @@
-import React, { useContext } from 'react'
-import { usersContext } from '../context/users/usersContext'
-import './styles/MisclanesProfile.css'
-import { ClanesAdministrados } from './ClanesAdministrados'
-
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom'; 
+import { usersContext } from '../context/users/usersContext';
+import './styles/MisclanesProfile.css';
+import { ClanesAdministrados } from './ClanesAdministrados';
 
 export const MisClanesProfile = () => {
-const {user} = useContext (usersContext)
+  const { user } = useContext(usersContext);
 
   return (
     <>
-    <div className='profileDatos' id='misClanesBox'>
-      <ul className='containerProfile' id='misClanesList'>
-        <li>
-        <h3>Hola {user.username}</h3>
-        </li>
-       
-       {
-        user.administradorde.map((clan)=>(
-          <ClanesAdministrados key={clan._id} id={clan._id}/>
-        ))
-       }
-      </ul>
-    </div>
+      <div className='profileDatos' id='misClanesBox'>
+        <ul className='containerProfile' id='misClanesList'>
+          <li>
+            <h3>Hola {user.username}</h3>
+          </li>
+          {user.administradorde.map((clan) => (
+            <ClanesAdministrados key={clan._id} id={clan._id} />
+          ))}
+         
+        </ul>
+        <div className="iniActions2">
+    
+            <NavLink to="/CrearClan">
+              <button>Crear un Clan⭐</button>
+            </NavLink>
+          </div>
+      </div>
+    
     </>
-  )
-}
+  );
+};
+
