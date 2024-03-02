f906f9edee35b33a263571301da4b7d29cbb3ca3
sebaao <saburto11@gmail.com>
2024-03-02 03:50:07 -0300

    [FEAT] - Add Member

--- src/pages/PagoExitoso.jsx
+++ src/pages/PagoExitoso.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import "./styles/Pagos.css";
+import axios from 'axios';
+import { usersContext } from '../context/users/usersContext';
 
 const PagoExitoso = () => {
-  const detalles = "Tu pago ha sido procesado correctamente.";
+const detalles = "Tu pago ha sido procesado correctamente.";
+const {user} = useContext(usersContext)
+const addMember = async () => {
+const url =`${import.meta.env.VITE_BACKENDURL}/clan/addmember`
+const {data} = await axios.post (url,{clanId: JSON.parse(localStorage.getItem("clanToJoin")),userId:user._id })
+console.log(data)
+
+  }
+  useEffect(()=>{
+addMember()
+
+  }, [])
 
   return (
     <div className="container">
